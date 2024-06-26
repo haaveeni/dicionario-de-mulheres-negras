cafb893d3618cd8c090d61f63a13a77ace32dfef
Veridiana Lucena <veridianalucena@gmail.com>
2024-06-26 19:23:35 -0300

    Added function carregarDicionario

---
+++ script.js
@@ -0,0 +1,13 @@
+function carregaDicionario() {
+    var biografias = [
+        "Mariele Franco", 
+        "Lelia Gonzalez",
+        "Angela Davis",
+        "Conceição Evaristo",
+        "Carolina de Jesus",
+        "Rosa Parks"
+    ];
+    alert(biografias);
+}
+
+carregaDicionario();
